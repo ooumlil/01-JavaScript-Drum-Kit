d641d1d313755e8fc74161e1af6b1fb9250cc6b9
Oussama Oumlil <hamza.oumlil.5@gmail.com>
2023-06-20 11:46:53 +0100

    done with the clicks on the keyboard to play sounds and make a small animation, to click on the div using the mouse and do the same as above

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-window.addEventListener('keydown', (e) => {
-	let audio = document.querySelector(`audio[data-key="${e.code}"]`);
-	if (!audio) return;
-	audio.play();
+window.addEventListener("keydown", (e) => {
+  let audio = document.querySelector(`audio[data-key="${e.code}"]`);
+  let keyPress = document.querySelector(`div[data-key="${e.code}"]`);
+  if (!audio || !keyPress) return;
+  audio.play();
+  keyPress.classList.add("playing");
 });
+
+window.addEventListener("keyup", e => {
+	let keyPress = document.querySelector(`div[data-key="${e.code}"]`);
+	if (!keyPress) return;
+  keyPress.classList.remove("playing");
+});
+
+// window.addEventListener("mouseenter", );
