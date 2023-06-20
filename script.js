81b693f6bd8d593fea5a6a590a9095802060d922
Oussama Oumlil <hamza.oumlil.5@gmail.com>
2023-06-20 14:12:00 +0100

    final version

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
-window.addEventListener("keydown", (e) => {
+window.addEventListener(`keydown`, (e) => {
   let audio = document.querySelector(`audio[data-key="${e.code}"]`);
   let keyPress = document.querySelector(`div[data-key="${e.code}"]`);
   if (!audio || !keyPress) return;
   audio.play();
-  keyPress.classList.add("playing");
+  keyPress.classList.add(`playing`);
 });
 
-window.addEventListener("keyup", e => {
-	let keyPress = document.querySelector(`div[data-key="${e.code}"]`);
-	if (!keyPress) return;
-  keyPress.classList.remove("playing");
+window.addEventListener(`keyup`, (e) => {
+  let keyPress = document.querySelector(`div[data-key="${e.code}"]`);
+  if (!keyPress) return;
+  keyPress.classList.remove(`playing`);
 });
-
-// window.addEventListener("mouseenter", );
